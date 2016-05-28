161bbeb77ce3e403be8f6f570590231bfa6f6842
ezebsas <mollinedoezequiel@gmail.com>
2016-05-28 01:15:08 -0300

    Comienzo de detalle de personajes, falta vincular con los commons...

--- src/main/webapp/js/angular-app/app.js
+++ src/main/webapp/js/angular-app/app.js
@@ -1,11 +1,13 @@
 angular.module('tacsthree.common', []);
 angular.module('tacsthree.user', ['ngResource', 'ui.bootstrap', 'tacsthree.common']);
 angular.module('tacsthree.characters', ['ngResource', 'ui.bootstrap', 'tacsthree.common']);
+angular.module('tacsthree.char', ['ngResource']);
 
 var tacsthreeApp = angular.module('tacsthree', [
   'tacsthree.common',
   'tacsthree.user',
   'tacsthree.characters',
+  'tacsthree.char',
   'ngResource',
   'ngRoute',
   'ui.bootstrap'
@@ -27,6 +29,10 @@ tacsthreeApp.config(['$routeProvider',
     .when('/characters', {
       templateUrl: 'templates/characters.html',
       controller: 'CharactersControllers'
+    })
+    .when('/characters/:idMarvel', {
+      templateUrl: 'templates/characters_detail.html',
+      controller: 'charactCtrl'
     })
     .otherwise({
       redirectTo: '/'
